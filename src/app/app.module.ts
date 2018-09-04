9e0402df65fedec17da2fd40f8b0cd63d951653b
Ben Solis <benjairsolis19@gmail.com>
2018-09-04 16:49:02 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonModule } from 'primeng/primeng';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/primeng';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    ButtonModule,
+    CardModule,
+    PanelModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
